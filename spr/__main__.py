bd582877bb07e297f4991c52c4d682c95a481fd5
rommelnita <93637393+rommelnita@users.noreply.github.com>
2021-11-11 08:59:35 +0700

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -49,7 +49,7 @@ async def help_command(_, message: Message):
     kb = ikb(
         {
             "Help": "bot_commands",
-            "Owner": "@FuckingMind_admin",
+            "Owner": "https://t.me/FuckingMind_admin",
             "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
             "Support Chat (for now)": "https://t.me/Gabutjoinsini",
         }
